8471b865ce848046d334fc50d231be63908d0cdb
mr-emeka <speak2c.emeka@gmail.com>
2018-07-01 07:24:47 +0100

    deleted config files

--- config/mailer.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    GMAIL_USER: 'speak2c.emeka@gmail.com',
-    CLIENT_ID: '753038429951-lhhmfjsimk7rijrsfopms7s57utp6c0o.apps.googleusercontent.com',
-    CLIENT_SECRET: '-f5BPL5iXDXs1RJ7rzvGm-de',
-    REFRESH_TOKEN: '1/IEQxJREcKnfooOVUhZ52wJpTkqzYSGkDI3S3NXnQ4a4'
-}
